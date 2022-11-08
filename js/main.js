d3d643e60908c0a0e03be490ea387a498d44baab
LAPTOP-Q0M3JGO4\gsthi <gs.thiago@hotmail.com>
2022-11-08 17:17:49 -0300

    Inicio - Procedimento de navegação

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const listagemTipo = "http://localhost/numerapub/Api/Buscartipos.php";
 //************************************************************************************ */
 window.onload = function () {
 
-  nomes = [];
+  const nomes = [];
 
   const menulogar = document.getElementById('logar');
   const menudocumentos = document.getElementById('documentos');
@@ -14,7 +14,16 @@ window.onload = function () {
 
   menulogar.addEventListener('click', function (e) {
 
-    alert("pressionou o LOGIN");
+     const opcaologar = document.getElementById('lista');
+
+     fetch('http://localhost/edoc-pmm/views/home.html')
+     .then((resposta) => resposta.text())
+     .then(function(html) {
+
+        opcaologar.innerHTML = html;
+
+     })
+     .catch((err) => console.log(err));
 
   });
 
@@ -32,3 +41,17 @@ window.onload = function () {
 
 };
 
+/*
+ // Initialize the DOM parser
+        var parser = new DOMParser();
+
+        // Parse the text
+        var doc = parser.parseFromString(html, "text/html");
+
+        // You can now even select part of that html as you would in the regular DOM 
+        // Example:
+        // var docArticle = doc.querySelector('article').innerHTML;
+
+       // console.log(doc);
+
+   */    
