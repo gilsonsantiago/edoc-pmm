33223e700413eb135f94c03f9b35bebafb38c10e
LAPTOP-Q0M3JGO4\gsthi <gs.thiago@hotmail.com>
2022-11-08 14:35:46 -0300

    Inicio - Procedimentos iniciais

--- js/funcoes.js
+++ js/funcoes.js
@@ -4,109 +4,130 @@ const listagemTipo = "http://localhost/numerapub/Api/Buscartipos.php";
 
 
 //************************************************************************************ */
-window.onload = function(){
+window.onload = function () {
 
-    nomes = [];
+  nomes = [];
 
-    const menulogar = document.getElementById('logar');
+  const menulogar = document.getElementById('logar');
+  const menudocumentos = document.getElementById('documentos');
+  const menurelatorios = document.getElementById('relatorios');
 
-    menulogar.addEventListener('click', function(e){
+  menulogar.addEventListener('click', function (e) {
 
-             alert("pressionou o login");
+    alert("pressionou o LOGIN");
 
-    });
+  });
+
+  menudocumentos.addEventListener('click', function (e) {
+
+    alert("pressionou o DOCUMENTOS");
+
+  });
+
+  menurelatorios.addEventListener('click', function (e) {
+
+    alert("pressionou o RELATORIOS");
+
+  });
+
+
+
 
 };
+
+
+
+
 
 
 //*********************************************************************************************** */
 function gravartipo() {
 
-    
-    const novoente      = document.getElementById("ente");
-    const novotipo      = document.getElementById("tipo");
-    const novadescricao = document.getElementById("descricao");
 
-   
-    tipos = {
-        'ente': novoente.value,
-        'tipo': novotipo.value,
-        'descricao': novadescricao.value
-    };
+  const novoente = document.getElementById("ente");
+  const novotipo = document.getElementById("tipo");
+  const novadescricao = document.getElementById("descricao");
 
-    nomes.push(tipos);
-    console.log(nomes);
-    
-    const options = {
-        method: "POST",
-        mode: "cors",
-        cache : "default",
-        headers: {
-            "Accept" : "application/json",
-            "Content-Type": "application/json; ",
-        },
-        body: (JSON.stringify(tipos)),
-    };
 
-    /********************************************** */
-    fetch(enderecoTipo, options)
-        .then(response => response.json())
-        .then(json => console.log(json))
-        .catch (err => console.log("erro... " + err));
-   
+  tipos = {
+    'ente': novoente.value,
+    'tipo': novotipo.value,
+    'descricao': novadescricao.value
+  };
+
+  nomes.push(tipos);
+  console.log(nomes);
+
+  const options = {
+    method: "POST",
+    mode: "cors",
+    cache: "default",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json; ",
+    },
+    body: (JSON.stringify(tipos)),
+  };
+
+  /********************************************** */
+  fetch(enderecoTipo, options)
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log("erro... " + err));
+
 
 };
 
 
 /******************************************************************* */
-function ler(){
+function ler() {
 
-     const listas = document.getElementById("lista");
-
-
-    /**************************************** */
-     function teste(dados){
-
-        const  div = document.createElement("div");
-        const  li  = document.createElement("li");
-        
-        div.setAttribute("align" , "justify");
-
-        li.innerText =  formataitem(dados.tipo) + ' -  ' + dados.descricao;
-
-        div.appendChild(li);
-        
-        listas.appendChild(div);
-
-      }
-
-      /********************************** */
-     fetch(listagemTipo)
-
-     .then((response) => {
-
-            return response.json();
-
-       })
-
-     .then((dados) => { 
-
-         dados.map((dados) => {
-
-           teste(dados);
-
-       });
-
-     });
+  const listas = document.getElementById("lista");
 
 
+  /**************************************** */
+  function teste(dados) {
+
+    const div = document.createElement("div");
+    const li = document.createElement("li");
+
+    div.setAttribute("align", "justify");
+
+    li.innerText = formataitem(dados.tipo) + ' -  ' + dados.descricao;
+
+    div.appendChild(li);
+
+    listas.appendChild(div);
+
+  }
+
+  /********************************** */
+  fetch(listagemTipo)
+
+    .then((response) => {
+
+      return response.json();
+
+    })
+
+    .then((dados) => {
+
+      dados.map((dados) => {
+
+        teste(dados);
+
+      });
+
+    });
 
 
-  };
 
-  /******************************************** */
-  function formataitem(item){
 
-    return ((item  < 10) ? '0' + item : item)
+};
 
-  };
+/******************************************** */
+function formataitem(item) {
+
+  return ((item < 10) ? '0' + item : item)
+
+};
